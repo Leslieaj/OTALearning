aea7d696960ba32b86d3070ade344fa02597d45d
leslie <ajstar1990@sina.com>
2019-03-12 20:16:54 +0800

    Add the definition of Finite Automata

---
+++ fa.py
@@ -0,0 +1,57 @@
+#The definition of the finite automaton
+
+class FALocation(object):
+    def __init__(self, name="", init=False, accept=False):
+        self.name = name
+        self.init = init
+        self.accept = accept
+
+    def __eq__(self, location):
+        if self.name == location.name and self.init == location.init and self.accept == location.accept:
+            return True
+        else:
+            return False
+            
+    def __hash__(self):
+        return hash(("FALOCATION", self.name, self.init, self.accept))
+
+    def get_name(self):
+        return self.name
+
+    def show(self):
+        return self.get_name() + ',' + str(self.init) + ',' + str(self.accept)
+
+class FATran(object):
+    def __init__(self, id, source="", target="", resettimedword=None):
+        self.id = id
+        self.source = source
+        self.target = target
+        self.resettimedword = resettimedword
+
+class FA:
+    def __init__(self, name="", rtw_alphabet = [], states = None, trans = [], initstate_name = "", accept_names = []):
+        self.name = name
+        self.rtw_alphabet = rtw_alphabet
+        self.states = states
+        self.trans = trans
+        self.initstate_name = initstate_name
+        self.accept_names = accept_names
+
+    def show(self):
+        print("FA name: ")
+        print(self.name)
+        print("alphabet:")
+        for term in self.rtw_alphabet:
+            print(term)
+        print("State (name, init, accept)")
+        for s in self.states:
+            print(s.name, s.init, s.accept)
+        print("transitions: (id, source, target, timedword): ")
+        for t in self.trans:
+            print(t.id, t.source, t.target, t.resettimedword)
+            #t.resettimedword.show()
+            #print
+        print("init state: ")
+        print(self.initstate_name)
+        print("accept states: ")
+        print(self.accept_names) 
